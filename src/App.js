5e2f4356ced181031f655a629d21280e38fddf3b
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2024-01-08 04:50:11 +0300

    initial setup :tada:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-400 p-2 m-3 rounded-lg"> 
+      <table className="border-separate border border-slate-500 ">
+        <thead>
+          <tr>
+            <th className="border border-slate-600 text-slate-500 bg-gray-400">State</th>
+            <th className="border border-slate-600 ">City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border border-slate-300 ">Indiana</td>
+            <td className="border border-slate-300 ">Indianapolis</td>
+          </tr>
+          <tr>
+            <td className="border border-slate-300 ">Ohio</td>
+            <td className="border border-slate-300 ">Columbus</td>
+          </tr>
+          <tr>
+            <td className="border border-slate-300 ">Michigan</td>
+            <td className="border border-slate-300 rounded ">Detroit</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
