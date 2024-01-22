5fb8cb2a9800a2db8ba0a39c9f5ecd6b0113c8a6
muchirajunior <muchirajunior022@gmail.com>
2024-01-22 21:32:31 +0300

    layout updates

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Navbar from '../components/navbar/navbar.tsx';
+import NavBar from './navbar.tsx';
 
 const Layout: React.FC<{ component: JSX.Element }> = ({ component }) => {
     return (
         <div className='flex flex-row flex-nowrap '>
-            <div className='sm:container '>
+            <div className='flex flex-col p-2 w-48 shadow h-screen '>
                 Side Nav
             </div>
-            <div className='flex flex-col'>
-                <Navbar navs={[]} title="REACT TAILWIND" />
+            <div className='flex flex-col w-full'>
+                <NavBar navs={[]} title="REACT TAILWIND" />
                 {component}
             </div>
 
--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -6,13 +6,10 @@ export type Navigation={
   link : string
 }
 
-const Navbar: React.FC<{navs: Navigation[]; title: String;}> =({navs, title}) => {
-  console.log(navs);
-   console.log(title);
-   
+const NavBar: React.FC<{navs: Navigation[]; title: String;}> =({navs, title}) => {
   return (
-    <div className='w-100 bg-slate-950 py-2 px-5 text-amber-50 flex-row'>
-      <span className='font-bold text-2xl'>REACT-TAILWIND</span>
+    <div className='w-auto shadow py-2 px-5 text-gray-600 flex-row sticky'>
+      <span className='font-bold text-2xl'>{title}</span>
       <span className=' flex-fill'>{
         navs.map((nav,index)=> <Link key={index} to={nav.navName}>{nav.navName}</Link> )
       }</span>
@@ -20,4 +17,4 @@ const Navbar: React.FC<{navs: Navigation[]; title: String;}> =({navs, title}) =>
   )
 }
 
-export default Navbar;
+export default NavBar;
