f75d42f1dc189e5496f109319b90bf64463be0fa
muchirajunior <muchirajunior022@gmail.com>
2024-02-13 03:57:41 +0300

    update side nav

--- src/layout/sidenav.tsx
+++ src/layout/sidenav.tsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 import { FaAppStoreIos } from "react-icons/fa6";
 
 export default function SideNav() {
-  const current=window.location.pathname;
-  
+  const current = window.location.pathname;
+
   return (
     <div className='flex flex-col justify-start h-full'>
-      <header className='m-3 flex flex-row '>  
-      <FaAppStoreIos className='p-2 m-1 bg-blue-600 text-white font-bold  text-3xl w-12 h-12 rounded me-2 ' /> 
-      <span>
-        <div className='text-2xl font-bold font-serif text-slate-600 '>CompanyX</div>
-        <div className='text-slate-400 ms-1'>dashboard</div>
-      </span>
-      
+      <header className='m-3 flex flex-row '>
+        <FaAppStoreIos className='p-2 m-1 bg-blue-600 text-white font-bold  text-3xl w-12 h-12 rounded me-2 ' />
+        <span>
+          <div className='text-2xl font-bold font-serif text-slate-600 '>CompanyX</div>
+          <div className='text-slate-400 ms-1'>dashboard</div>
+        </span>
+
       </header>
       <hr />
       <span className='h-5'></span>
@@ -23,7 +23,8 @@ export default function SideNav() {
           <Link
             to={route.path}
             key={route.path}
-            className={`flex flex-row align-middle rounded m-1 bg-gray-100 hover:bg-blue-600 hover:text-white p-2 font-bold text-lg ${route.path===current ? "bg-blue-700 text-white":null}`}
+            className={`flex flex-row align-middle rounded m-1 bg-gray-100 hover:bg-blue-600 hover:text-white p-2 font-bold text-lg 
+              ${route.path === current ? "bg-blue-700 text-white" : null}`}
           >
             {<route.icon className=' mx-2 mt-1' />}
             {route.name}
